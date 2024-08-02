8848543e24a3c51a64859305d9c902d6e8242756
Scott <SOEZAW.23@ichat.sp.edu.sg>
2024-08-02 16:26:17 +0800

    change the app file

--- src/app.py
+++ src/app.py
@@ -1,2 +1,16 @@
 # Created on : 1 Aug 2024, 11:39:05 pm
 # Author     : myatt
+# pip install streamlit langchain lanchain-openai beautifulsoup4 python-dotenv chromadb
+
+import streamlit as st
+
+
+# app config
+st.set_page_config(page_title="Chat with websites", page_icon="🤖")
+st.title("Chat with websites")
+
+# sidebar
+with st.sidebar:
+    st.header("Settings")
+    website_url = st.text_input("Website URL")
+
